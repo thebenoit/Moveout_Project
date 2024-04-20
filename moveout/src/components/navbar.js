4c9501b842431a64ec5cf4918d6aa90979d6ad6a
thebenoit <kemmywillbenoit@yahoo.com>
2024-04-20 15:48:58 -0400

    installer react-dom

---
+++ moveout/src/components/navbar.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+
+function navbar() {
+  return (
+    <>
+       <nav className='navbar'>
+
+       <div className='navbar-container'>
+        <Link to="/" className="navbar-logo"></Link>
+
+       </div>
+
+
+       </nav>
+             
+        
+       
+    </>
+  )
+}
+
+export default navbar
