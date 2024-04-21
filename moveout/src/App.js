85ff14b50cf2c3c791ca53bd06010db8c6a4c4a9
thebenoit <kemmywillbenoit@yahoo.com>
2024-04-21 14:51:59 -0400

    je sais plus ce que j'ai modifié tabarnak

--- moveout/src/App.js
+++ moveout/src/App.js
@@ -1,6 +1,6 @@
 //import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 import React from 'react';
 //import logoSource from './'
--- moveout/src/components/navbar.js
+++ moveout/src/components/navbar.js
@@ -2,28 +2,52 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import MoveoutLogo from './Logo'
 
-function navbar() {
-    //const [click,setClick] = useState(false)
+function Navbar() {
+const [click,setClick] = useState(false)
+
+const handleClick = () => setClick(!click)
+const closeMobileMenu = () => setClick(false)
 const imageSrc = '/Image_Logos/moveoutLogo2.svg'
   return (
     <>
        <nav className='navbar'>
 
        <div className='navbar-container'>
-       <Link to='/' className='navbar-logo'>
-                TRVL <i className='fab fa-typo3'/>
-            </Link>
+      
 
             <MoveoutLogo
                 titre={"Moveout"}
                 imageSrc={imageSrc}
 
             />
-        <div className='Menu-Icon'>
-           {/**<i className={click ? 'fas fa-time' : 'fas fa-bars'}/>*/}
+        <div className='Menu-Icon' onClick={handleClick}>
+           <i className={click ? 'fa-solid fa-xmark' : 'fas fa-bars'}/>
             
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+         <li className='nav-item'>
+            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Home
+            </Link>
+            
+            
+            
 
+           
+        </li>   
+        <li><Link to='/' className='nav-links' onClick={closeMobileMenu}>
+               Déménager
+            </Link></li>
+        <li><Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                About us
+            </Link></li>
+        <li>
+        <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                Sign Up
+            </Link>
+        </li>
+        
+        </ul>
        </div>
 
 
@@ -35,4 +59,4 @@ const imageSrc = '/Image_Logos/moveoutLogo2.svg'
   )
 }
 
-export default navbar
+export default Navbar
