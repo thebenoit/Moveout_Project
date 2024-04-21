b9f25fe8041b43eccdbe4c0a5d5471986b1e4dc4
thebenoit <kemmywillbenoit@yahoo.com>
2024-04-20 21:42:03 -0400

    ajour du Logg moveout

--- moveout/src/components/Logo.js
+++ moveout/src/components/Logo.js
@@ -4,7 +4,7 @@ import './Logo.css';
 function Logo({titre, imageSrc}) {
   return (
     <div className="logo-container">
-      <img src={imageSrc} alt="Logo" ClassName="logo-img" />
+      <img src={imageSrc} alt="Logo" className="logo-img" />
       <span className="logo-text">{titre}</span>
     </div>
   );
