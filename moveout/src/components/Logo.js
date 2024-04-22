1a261bd5e5cb17ae3432f8bf3a6a78a9d703e7f9
thebenoit <kemmywillbenoit@yahoo.com>
2024-04-21 23:23:23 -0400

    corriger menu dans version mobile

--- moveout/src/components/Logo.js
+++ moveout/src/components/Logo.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './Logo.css';
 
-function Logo({titre, imageSrc}) {
+function Logo({titre, imageSrc, action}) {
   return (
-    <div className="logo-container">
+    <div className="logo-container" onClick={action}>
       <img src={imageSrc} alt="Logo" className="logo-img" />
       <span className="logo-text">{titre}</span>
+      
     </div>
   );
 }
--- moveout/src/components/navbar.js
+++ moveout/src/components/navbar.js
@@ -1,76 +1,78 @@
-import React, {useState} from 'react'
-import {Link} from 'react-router-dom'
-import MoveoutLogo from './Logo'
-import {Button} from './Button.js'
-import './navbar.css'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import MoveoutLogo from "./Logo";
+import { Button } from "./Button.js";
+import "./navbar.css";
 
 function Navbar() {
-const [click,setClick] = useState(false)
-const [button, setButton] = useState(true);
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
-const handleClick = () => setClick(!click)
-const closeMobileMenu = () => setClick(false)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-const showButton = () => {
-    if(window.innerWidth <= 960) {
-        setButton(false);
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
     } else {
-        setButton(true);
+      setButton(true);
     }
-};
-window.addEventListener('resize', showButton);
-const imageSrc = '/Image_Logos/moveoutLogo2.svg'
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+  const imageSrc = "/Image_Logos/moveoutLogo2.svg";
   return (
     <>
-       <nav className='navbar'>
+      <nav className="navbar">
+        <div className="navbar-container">
 
-       <div className='navbar-container'>
-      
+          <MoveoutLogo 
+          titre={"Moveout"} 
+          imageSrc={imageSrc}
+          action={closeMobileMenu} />
 
-            <MoveoutLogo
-                
-                titre={"Moveout"}
-                imageSrc={imageSrc}
-
-            />
-        <div className='Menu-Icon' onClick={handleClick}>
-           <i className={click ? 'fa-solid fa-xmark' : 'fas fa-bars'}/>
-            
-        </div>
-        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-         <li className='nav-item'>
-            <Link to='/' className='nav-links-mobile' onClick={closeMobileMenu}>
-                Home
-            </Link>
-            
-            
-            
-
-           
-        </li>   
-        <li className='nav-item' ><Link to='/demenagement' className='nav-links-mobile' onClick={closeMobileMenu}>
-               Déménager
-            </Link></li>
-        <li className='nav-item'><Link to='/about-us' className='nav-links-mobile' onClick={closeMobileMenu}>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fa-solid fa-xmark" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link
+                to="/demenagement"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Déménager
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/about-us"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 About us
-            </Link></li>
-        <li className='nav-item'>
-        <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+              </Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
                 Sign Up
-            </Link>
-        </li>
-        
-        </ul>
-        {button && <Button buttonStyles='btn--outline'> Sign Up</Button>}
-       </div>
-
-
-       </nav>
-             
-        
-       
+              </Link>
+            </li>
+          </ul>
+          {/*button && <Button buttonStyles="btn--outline"> Sign Up</Button>*/}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
